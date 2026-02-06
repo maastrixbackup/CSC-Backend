c2ffd32ce76eebf129ed93b4d3a36ff96b81c218
pravatmaastrix <pravat.maastrix@gmail.com>
2026-02-06 13:04:29 +0530

    Token expire logic removed

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,7 +4,7 @@ const generateToken = (user, expiresIn = "1h") => {
     return jwt.sign(
         { id: user.id, role: user.role },
         process.env.JWT_SECRET,
-        { expiresIn }
+        // { expiresIn }
     );
 };
 
